48c04d43e127cf6d9a56f111072a7d67d42d5cf3
Igor Drobiazko <igor@elastic.io>
2017-03-07 12:23:46 +0100

    Proper args parsing

--- vendor/compatibility/validate.js
+++ vendor/compatibility/validate.js
@@ -1,12 +1,19 @@
 var semver = require('semver');
+var program = require('commander');
 
-var nodeJsVersion = process.argv[2];
-var sailorVersion = process.argv[3];
+program
+    .version('0.0.1')
+    .option('-n, --node [value]', 'Node.js version')
+    .option('-s, --sailor [value]', 'Sailor version')
+    .parse(process.argv);
+
+var nodeJsVersion = program.node;
+var sailorVersion = program.sailor;
 
 var NPM_PACKAGE = 'https://www.npmjs.com/package/elasticio-sailor-nodejs';
 
 if (!sailorVersion) {
-    throw new Error('Please define a dependency to elasticio-sailor-nodejs. '
+    throw new Error('Please define a dependency to elasticio-sailor-nodejs.'
         + 'For more details see ' + NPM_PACKAGE);
 }
 
@@ -17,7 +24,7 @@ if (semver.lt(sailorVersion, '1.3.0')) {
 }
 
 if (semver.gte(sailorVersion, '2.0.0')) {
-    if (semver.lt(nodeJsVersion, '6.0.0')) {
+    if (nodeJsVersion && semver.lt(nodeJsVersion, '6.0.0')) {
 
         throw new Error('You are using elasticio-sailor-nodejs@'
             + sailorVersion
